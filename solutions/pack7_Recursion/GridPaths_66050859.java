20d54280fc1ef29d4f8df23fffdbf310c3a07818
Boby <boblaveyou@gmail.com>
2024-08-20 22:22:39 +0700

    lab7

--- solutions/pack7_Recursion/GridPaths_66050859.java
+++ solutions/pack7_Recursion/GridPaths_66050859.java
@@ -3,34 +3,30 @@ package solutions.pack7_Recursion;
 public class GridPaths_66050859 {
 
     public static int numberOfPaths(int grid [][]) {
+        // check
         if (grid == null || grid.length == 0 || grid[0].length == 0)
             return 0;
         
-            int m = grid.length;
-            int n = grid[0].length;
-            int[][] dp = new int[m][n];
-            return countPaths(grid, 0, 0, dp);
+        return countPaths(grid, 0, 0);
     }
 
-    private static int countPaths(int[][] grid, int i, int j, int[][] dp) {
+    private static int countPaths(int[][] grid, int i, int j) {
         int m = grid.length;
         int n = grid[0].length;
 
-        // Base
+        // Base when overflow or hit obstacle
         if (i >= m || j >= n || grid[i][j] == 1) {
             return 0;
         }
 
-        // end
+        // hit end
         if (i == m - 1 && j == n - 1) {
             return 1;
         }
-        if (dp[i][j] != 0) {
-            return dp[i][j];
-        }
-        int rightPaths = countPaths(grid, i, j + 1, dp);
-        int downPaths = countPaths(grid, i + 1, j, dp);
-        dp[i][j] = rightPaths + downPaths;
-        return dp[i][j];
+
+        // traverse via recursion
+        int rightPaths = countPaths(grid, i, j + 1);
+        int downPaths = countPaths(grid, i + 1, j);
+        return rightPaths + downPaths;
     }
 }
